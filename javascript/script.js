297168449dcf197bcd26d9afb8805c05ad1d8771
kushyzee <niklaus.oa@gmail.com>
2023-12-29 12:45:52 +0100

    updated all files

--- javascript/script.js
+++ javascript/script.js
@@ -4,10 +4,14 @@ const closeBtn = document.getElementById("close-btn");
 const navbarLinks = document.getElementById("navbar-links");
 const triggerArea = document.getElementById("trigger-area");
 const dialogBox = document.getElementById("dialog");
+const seeAllBtn = document.getElementById("profile-link");
+const seeAllDialog = document.getElementById("see-all-dialog");
+
 const form = document.forms[0];
+
 const [name, email, subject, message, button] = form;
 
-console.log(dialogBox.children); 
+// console.log(seeAllDialog);
 
 const openNavBar = () => {
 	navbar.classList.remove("hidden");
@@ -20,19 +24,18 @@ const closeNavBar = () => {
 };
 
 const openDialogBox = (e) => {
-	// console.log(e.target.id)
 	e.preventDefault();
 	if (e.target === button) {
 		dialogBox.showModal();
 	}
-}
+};
 
 const clearForm = () => {
 	name.value = '';
 	email.value = '';
 	subject.value = '';
 	message.value = '';
-}
+};
 
 const submitForm = (e) => {
 	if (e.target.id === 'cancel-btn') {
@@ -42,8 +45,13 @@ const submitForm = (e) => {
 	else if (e.target.id === 'submit-btn') {
 		form.submit()
 		clearForm()
+		dialogBox.close()
 	}
-}
+};
+
+const openSeeAllDialog = () => seeAllDialog.showModal()
+
+const githubSite = (e) => e.target.localName === 'button' ? seeAllDialog.close() : pass
 
 openBtn.addEventListener("click", openNavBar);
 closeBtn.addEventListener("click", closeNavBar);
@@ -52,3 +60,5 @@ triggerArea.addEventListener("click", closeNavBar);
 triggerArea.addEventListener("touchstart", closeNavBar);
 form.addEventListener("click", openDialogBox);
 dialogBox.addEventListener("click", submitForm);
+seeAllBtn.addEventListener('click', openSeeAllDialog);
+seeAllDialog.addEventListener('click', githubSite);
